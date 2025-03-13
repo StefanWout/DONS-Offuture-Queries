5cef258aa80f4c9bfabef33b86d436e89c6cf9f2
nav17 <cayendo1@gmail.com>
2025-03-13 15:23:28 +0000

    Updated

--- dons_queries-nav.sql
+++ dons_queries-nav.sql
@@ -6,7 +6,7 @@ SELECT
     a.city,
     a.country
 FROM
-     offuture.order AS o
+     offuture."order" AS o
 INNER JOIN offuture.address AS a
     ON a.address_id = o.address_id
 GROUP BY
@@ -23,7 +23,7 @@ SELECT
     SUM(oi.profit)
 FROM 
     offuture.product AS p
-INNER JOIN order_item AS oi
+INNER JOIN offuture.order_item AS oi
     ON p.product_id = oi.product_id
 GROUP BY
     product_name_no_colour
@@ -32,18 +32,8 @@ ORDER BY
 LIMIT
     10
 ;
-
-SELECT DISTINCT
-    order_priority
-FROM
-    offuture.order;
-
-SELECT DISTINCT
-    segment
-FROM
-    customer
     
--- Total units sold by sub category
+-- Sub Category Aggregations
     
 SELECT
     p.sub_category,
@@ -52,10 +42,12 @@ SELECT
     ROUND(AVG(oi.quantity),2) AS avg_units_per_order,
     SUM(oi.profit) AS total_profit,
     SUM(oi.sales) AS total_sales,
-    o.order_date
+    p.category,
+    c.segment,
+    o.order_priority
 FROM
-    customer AS c
-INNER JOIN offuture.order AS o
+    offuture.customer AS c
+INNER JOIN offuture."order" AS o
     ON c.customer_id_long = o.customer_id
 INNER JOIN offuture.order_item AS oi
     ON oi.order_id = o.order_id
@@ -63,9 +55,11 @@ INNER JOIN offuture.product AS p
     ON p.product_id = oi.product_id
 GROUP BY
     p.sub_category,
-    o.order_date
+    p.category,
+    c.segment,
+    o.order_priority
 ORDER BY
-    avg_profit DESC;
+    total_profit DESC;
 
 -- Top performing Copiers (highest profit sub category)
 
@@ -85,14 +79,33 @@ GROUP BY
 ORDER BY
     total_profit DESC;
     
+-- Top performing Copiers (no variations)
 
+SELECT
+    SPLIT_PART(p.product_name, ',', 1) AS product_name_no_colour,
+    ROUND(AVG(oi.profit),2) AS avg_profit,
+    SUM(oi.profit) AS total_profit,
+    SUM(oi.quantity) AS total_qty
+FROM
+    offuture.product AS p
+INNER JOIN offuture.order_item AS oi
+    ON oi.product_id = p.product_id
+    WHERE
+    p.sub_category = 'Copiers'
+GROUP BY
+    product_name_no_colour
+ORDER BY
+    total_profit DESC;
+
+
+-- Top 10 Products (no variations)
 
 SELECT 
     SPLIT_PART(p.product_name, ',', 1) AS product_name_no_colour,
     SUM(oi.profit)
 FROM 
     offuture.product AS p
-INNER JOIN order_item AS oi
+INNER JOIN offuture.order_item AS oi
     ON p.product_id = oi.product_id
 GROUP BY
     product_name_no_colour
@@ -106,13 +119,17 @@ LIMIT
 
 SELECT 
     p.product_name,
+    p.category,
+    p.sub_category,
     SUM(oi.profit)
 FROM 
     offuture.product AS p
-INNER JOIN order_item AS oi
+INNER JOIN offuture.order_item AS oi
     ON p.product_id = oi.product_id
 GROUP BY
-    p.product_name
+    p.product_name,
+    p.category,
+    p.sub_category
 ORDER BY
     SUM(oi.profit) DESC
 LIMIT 10
@@ -125,22 +142,142 @@ SELECT DISTINCT
 FROM
     offuture.product;
 
-select
-    p.product_name,
-    sum(o.profit),
-    p.category
-from
-    product p
-inner join order_item o on p.product_id = o.product_id
-group by
-    p.product_name,
-    p.category
-order by
-    sum(o.profit) desc
-limit 10;
+-- Aggregations of Customer segments with category
 
-
-SELECT DISTINCT
-    region
+SELECT
+    c.segment,
+    SUM(oi.quantity) AS total_units,
+    ROUND(AVG(oi.quantity),2) AS avg_units_per_order,
+    SUM(oi.profit) AS total_profit,
+    ROUND(AVG(oi.profit),2) AS avg_profit,
+    SUM(oi.sales) AS total_sales,
+    p.category
 FROM
-    offuture.order
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+GROUP BY
+    c.segment,
+    p.category
+ORDER BY
+    c.segment;
+
+-- Aggregations of Customer segments
+
+SELECT
+    c.segment,
+    SUM(oi.quantity) AS total_units,
+    ROUND(AVG(oi.quantity),2) AS avg_units_per_order,
+    SUM(oi.profit) AS total_profit,
+    ROUND(AVG(oi.profit),2) AS avg_profit,
+    SUM(oi.sales) AS total_sales
+FROM
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+GROUP BY
+    c.segment
+ORDER BY
+    c.segment;
+
+-- Aggregations of Sub Category
+
+SELECT
+    p.sub_category,
+    SUM(oi.quantity) AS total_units,
+    ROUND(AVG(oi.quantity),2) AS avg_units_per_order,
+    SUM(oi.profit) AS total_profit,
+    ROUND(AVG(oi.profit),2) AS avg_profit,
+    SUM(oi.sales) AS total_sales
+FROM
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+GROUP BY
+    p.sub_category
+ORDER BY
+    total_profit;
+
+-- Aggregations of Tables
+
+SELECT
+    p.product_name,
+    SUM(oi.quantity) AS total_units,
+    ROUND(AVG(oi.quantity),2) AS avg_units_per_order,
+    SUM(oi.profit) AS total_profit,
+    ROUND(AVG(oi.profit),2) AS avg_profit,
+    SUM(oi.sales) AS total_sales
+FROM
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+    WHERE 
+        p.sub_category = 'Tables'
+GROUP BY
+    p.product_name
+ORDER BY
+    total_profit DESC;
+
+-- All orders containing Tables
+
+SELECT
+    o.order_id,
+    p.product_name,
+    oi.discount,
+    oi.quantity,
+    oi.profit,
+    oi.sales
+FROM
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+    WHERE 
+        p.sub_category = 'Tables'
+ORDER BY
+    oi.profit DESC;
+
+
+-- All Orders 
+
+SELECT
+    DISTINCT o.order_id,
+    p.product_name,
+    p.category,
+    p.sub_category,
+    oi.discount,
+    oi.quantity,
+    oi.profit,
+    oi.sales
+FROM
+    offuture.customer AS c
+INNER JOIN offuture.order AS o
+    ON c.customer_id_long = o.customer_id
+INNER JOIN offuture.order_item AS oi
+    ON oi.order_id = o.order_id
+INNER JOIN offuture.product AS p
+    ON p.product_id = oi.product_id
+INNER JOIN offuture.address AS a
+    ON a.address_id = o.address_id
+ORDER BY
+    oi.profit DESC;
+
